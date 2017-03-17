63c1cb7cad3ec162bd44993ee0226cc3586fc894
denniscarr <dcarr1@gmail.com>
2017-03-16 22:04:24 -0400

    Added some new music and fixed a bug where the same track would play over and over.

--- Static/Assets/Scripts/MusicScript.cs
+++ Static/Assets/Scripts/MusicScript.cs
@@ -4,18 +4,27 @@ using System.Collections;
 public class MusicScript : MonoBehaviour {
 
 	public AudioClip[] tracks;
-	AudioSource audioSource;
+    AudioSource audioSource;
+    int lastIndex;
 
 	void Start () {
 		audioSource = GetComponent<AudioSource> ();
 		int clipIndex = Random.Range(0, tracks.Length);
+        lastIndex = clipIndex;
 		audioSource.clip = tracks [clipIndex];
 	}
 
 	void ChooseNewClip()
-	{
-		int clipIndex = Random.Range(0, tracks.Length);
-		audioSource.clip = tracks [clipIndex];
+    {
+        // Make sure the same track does not play twice in a row.
+        int clipIndex = lastIndex;
+        while (clipIndex == lastIndex)
+        {
+            clipIndex = Random.Range(0, tracks.Length);
+        }
+
+        lastIndex = clipIndex;
+        audioSource.clip = tracks [clipIndex];
 		audioSource.Play ();
 	}
 	
